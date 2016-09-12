d4ba2bf146c7c0edcd67669f2d1b8b4395d8eb05
Elysia Avery Nason <elysia.avery@gmail.com>
2016-09-11 18:49:08 -0700

    Update Shelter.java
    add new java

--- animal-shelter/Shelter.java
+++ animal-shelter/Shelter.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class Shelter {
   public static void main(String[] args) {
     Console myConsole = System.console();
-    Animal cat = new Animal("Lucy", 10, "Calico", "Cat", "Turtle", 50, "Not Adopted");
-    Animal dog = new Animal("Felix", 7, "Shar Pei", "Dog", "Apricot", 100, "Not Adopted");
-    Animal iguana = new Animal("Gomez", 10, "Cuban Rock Iguana", "Lizard", "Dark Green", 75, "Not Adopted");
-    Animal bird = new Animal("Morticia", 1, "Canary", "Bird", "Yellow", 50, "Not Adopted");
+    Animal cat = new Animal("Lucy", 10, "Calico", "Cat", "Turtle", 50);
+    Animal dog = new Animal("Felix", 7, "Shar Pei", "Dog", "Apricot", 100);
+    Animal iguana = new Animal("Gomez", 10, "Cuban Rock Iguana", "Lizard", "Dark Green", 75);
+    Animal bird = new Animal("Morticia", 1, "Canary", "Bird", "Yellow", 50);
 
     List<Animal> allAnimals = new ArrayList<Animal>();
     allAnimals.add(cat);
@@ -20,7 +20,6 @@ public class Shelter {
 
     while(programRunning){
       System.out.println("Welcome to Elysia's Animal Shelter! Enter one of the following options: All Animals, Search by Age, Search by Adoption, Add Animal, or Exit");
-
       String navigationChoice = myConsole.readLine();
 
     if(navigationChoice.equals("All Animals")){
@@ -61,7 +60,7 @@ public class Shelter {
               System.out.println("We're sorry! They were adopted!");
             } else {
               adoptee = userPick;
-              userPick.gotAdopted();
+              userPick.worthAdopted();
             }
           }
         }
@@ -87,12 +86,12 @@ public class Shelter {
       Animal userAnimal = new Animal(userInputName, userInputAge, userInputBreed, userInputAnimalType, userInputColor, userInputPrice);
       System.out.println("Here is the animal that you have put up for adoption.");
       System.out.println( "----------------------" );
-      System.out.println(userInputName.mName);
-      System.out.println(userInputAge.mAge);
-      System.out.println(userInputBreed.mBreed);
-      System.out.println(userInputAnimalType.mAnimalType);
-      System.out.println(userInputColor.mColor);
-      System.out.println(userInputPrice.mPrice);
+      System.out.println(userInputName);
+      System.out.println(userInputAge);
+      System.out.println(userInputBreed);
+      System.out.println(userInputAnimalType);
+      System.out.println(userInputColor);
+      System.out.println(userInputPrice);
   } else if (navigationChoice.equals("Exit")){
       programRunning = false;
   } else {
@@ -101,3 +100,4 @@ public class Shelter {
   }
   }
 }
+
